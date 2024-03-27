2adf6aba6dd9432a49da5e57a6313ed424ea0da2
FelipeMCassiano <felipemcassiano@gmail.com>
2024-03-26 21:09:58 -0300

    feat: change the connection to the database to a function

--- repository/repository.ts
+++ repository/repository.ts
@@ -1,12 +1,16 @@
 import pg from "pg";
 
-const pool = new pg.Pool({
-    user: "todo",
-    host: "localhost",
-    database: "todo",
-    password: "todo",
-    port: 5432,
-});
+async function connectDb(): Promise<pg.PoolClient> {
+    const pool = new pg.Pool({
+        user: "todo",
+        host: "localhost",
+        database: "todo",
+        password: "todo",
+        port: 5432,
+    }).connect();
+
+    return pool;
+}
 
 export interface ITodo {
     title: string;
@@ -27,7 +31,7 @@ export interface ITodoResponse {
 }
 
 export async function findById(id: number): Promise<ITodo | null> {
-    const conn = await pool.connect();
+    const conn = await connectDb();
 
     const rows = await conn.query<ITodo>(
         "SELECT title, description, completed FROM todo WHERE id =$1",
@@ -42,7 +46,7 @@ export async function findById(id: number): Promise<ITodo | null> {
 }
 
 export async function createTodo(todo: ITodoRequest): Promise<ITodoResponse | null> {
-    const conn = await pool.connect();
+    const conn = await connectDb();
 
     const rows = await conn.query<ITodoResponse>(
         "INSERT INTO todo (title, description) VALUES($1, $2) RETURNING id, title, description, completed",
@@ -55,7 +59,7 @@ export async function createTodo(todo: ITodoRequest): Promise<ITodoResponse | nu
 }
 
 export async function updateTodo(id: number, todo: ITodo): Promise<ITodoResponse | null> {
-    const conn = await pool.connect();
+    const conn = await connectDb();
 
     const rows = await conn.query<ITodoResponse>(
         "UPDATE todo SET title =$1, description=$2, completed=$3 WHERE id =$4 RETURNING id, title, description, completed",
@@ -68,7 +72,7 @@ export async function updateTodo(id: number, todo: ITodo): Promise<ITodoResponse
 }
 
 export async function deleteTodo(id: number): Promise<void | null> {
-    const conn = await pool.connect();
+    const conn = await connectDb();
 
     const rows = await conn.query("DELETE FROM todo WHERE id=$1", [id]);
     if (rows.rowCount === 0) return null;
--- src/index.ts
+++ src/index.ts
@@ -8,56 +8,60 @@ import {
     updateTodo,
 } from "../repository/repository";
 
-const app: Express = express();
-const port: Number = 3000;
+function main() {
+    const app: Express = express();
+    const port: Number = 3000;
 
-app.use(express.json());
+    app.use(express.json());
 
-app.get("/todo/:id", async (req: Request, res: Response) => {
-    const id = parseInt(req.params.id);
+    app.get("/todo/:id", async (req: Request, res: Response) => {
+        const id = parseInt(req.params.id);
 
-    const todo = await findById(id);
+        const todo = await findById(id);
 
-    if (!todo) return res.status(404).end();
+        if (!todo) return res.status(404).end();
 
-    return res.status(200).send(todo);
-});
+        return res.status(200).send(todo);
+    });
 
-app.post("/todo/create", async (req: Request, res: Response) => {
-    const todo: ITodoRequest = req.body;
+    app.post("/todo/create", async (req: Request, res: Response) => {
+        const todo: ITodoRequest = req.body;
 
-    if (!todo.title || !todo.description) return res.status(400).send("param missing");
+        if (!todo.title || !todo.description) return res.status(400).send("param missing");
 
-    const createdTodo = await createTodo(todo);
+        const createdTodo = await createTodo(todo);
 
-    if (!createTodo) return res.status(422).send("cannot create new todo").end();
+        if (!createTodo) return res.status(422).send("cannot create new todo").end();
 
-    return res.status(201).json({ newTodo: createdTodo });
-});
+        return res.status(201).json({ newTodo: createdTodo });
+    });
 
-app.put("/todo/update/:id", async (req: Request, res: Response) => {
-    const id = parseInt(req.params.id);
-    const todo: ITodo = req.body;
+    app.put("/todo/update/:id", async (req: Request, res: Response) => {
+        const id = parseInt(req.params.id);
+        const todo: ITodo = req.body;
 
-    const updatedTodo = await updateTodo(id, todo);
+        const updatedTodo = await updateTodo(id, todo);
 
-    if (!updatedTodo) return res.status(422).json({ error: "cannot updated todo" });
+        if (!updatedTodo) return res.status(422).json({ error: "cannot updated todo" });
 
-    return res.status(200).json({ success: updatedTodo });
-});
+        return res.status(200).json({ success: updatedTodo });
+    });
 
-app.delete("/todo/delete/:id", async (req: Request, res: Response) => {
-    const id = parseInt(req.params.id);
-    try {
-        const deleted = await deleteTodo(id);
-        if (deleted === null) return res.status(422).send("todo already deleted");
+    app.delete("/todo/delete/:id", async (req: Request, res: Response) => {
+        const id = parseInt(req.params.id);
+        try {
+            const deleted = await deleteTodo(id);
+            if (deleted === null) return res.status(422).send("todo already deleted");
 
-        return res.status(200).send("delete successfull").end();
-    } catch (err) {
-        return res.status(500).json({ error: err }).end();
-    }
-});
+            return res.status(200).send("delete successfull").end();
+        } catch (err) {
+            return res.status(500).json({ error: err }).end();
+        }
+    });
 
-app.listen(port, () => {
-    console.log(`[server]: Server is running at http://localhost:${port}`);
-});
+    app.listen(port, () => {
+        console.log(`[server]: Server is running at http://localhost:${port}`);
+    });
+}
+
+main();
